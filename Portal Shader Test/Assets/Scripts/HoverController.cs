065458ea21dd16b9b44f85cfdc24ec86a9594e4c
tkidder <no.name@google.com>
2019-10-26 11:53:25 -0500

    initial commit

---
+++ Portal Shader Test/Assets/Scripts/HoverController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HoverController : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    private Vector3 startMarker;
+    private float delay;
+    public float amplitude = .25f;
+    public float period = 2.5f;
+ 
+    protected void Start() {
+        startMarker = gameObject.transform.position;
+        delay = Random.Range(0, 15);
+    }
+ 
+    protected void Update() {
+        float theta = (Time.timeSinceLevelLoad+delay) / period;
+        float distance = amplitude * Mathf.Sin(theta);
+        transform.position = startMarker + Vector3.up * distance;
+    }
+
+    float CalculateRandomHeight() {
+        return Random.Range(0, 1f);
+    }
+}
